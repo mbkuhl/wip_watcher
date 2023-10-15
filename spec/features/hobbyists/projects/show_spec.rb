eeef5e1ae46785b7a0643bdc771bb67dea7400ad
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-10-14 19:48:08 -0600

    Implement feature to satisfy hobbyist projects index page portion of user story 18

--- spec/features/hobbyists/projects/show_spec.rb
+++ spec/features/hobbyists/projects/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Hobbyists Show", type: :feature do
+RSpec.describe "Hobbyists Show Projects", type: :feature do
 
   before(:each) do
     @hobbyist1 = Hobbyist.create!(name: "Mike", weekly_free_hours: 10, disposible_income: 1000, has_kids: false)
@@ -57,6 +57,30 @@ RSpec.describe "Hobbyists Show", type: :feature do
           expect("Crochet Scarf").to appear_before("Learn Coding")
           expect("Learn Coding").to appear_before("Make Ramen")
           expect("Make Ramen").to appear_before("Replace Exterior Door")
+        end
+      end
+    end
+
+# User Story 18, Child Update From Childs Index Page 
+
+# As a visitor
+# When I visit the `child_table_name` index page or a parent `child_table_name` index page
+# Next to every child, I see a link to edit that child's info
+# When I click the link
+# I should be taken to that `child_table_name` edit page where I can update its information just like in User Story 14
+
+    describe "when I visit '/hobbyists/:id/projects/', next to every project, I see a link to edit that project's info" do
+      it "When I click the link, I should be taken to that project's edit page where I can update its information" do
+        hobbyists = Hobbyist.all
+        hobbyists.each do |hobbyist|
+          visit "/hobbyists/#{hobbyist.id}/projects"
+          projects = hobbyist.projects
+          projects.each do |project|
+            expect(page).to have_link("Update #{project.project_name}", href: "/projects/#{project.id}/update")
+            click_link("Update #{project.project_name}")
+            expect(current_path).to eq("/projects/#{project.id}/update")
+            visit "/hobbyists/#{hobbyist.id}/projects"
+          end
         end
       end
     end
