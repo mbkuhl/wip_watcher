dc49bb579cfcb31d0110e260d3e80f189643d853
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-10-14 20:36:11 -0600

    Add test for user story 19

--- spec/features/hobbyists/show_spec.rb
+++ spec/features/hobbyists/show_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe "Hobbyists Show", type: :feature do
 # When I visit a parent show page ('/parents/:id')
 # Then I see a link to take me to that parent's `child_table_name` page ('/parents/:id/child_table_name')
 
-  describe "As a visitor" do
+
     describe "When I visit a hobbyist show page ('hobbyist/:id')" do
       it "Then I see a link to take me to that hobbysist's project page ('/hobbyists/:id/projects)" do
         visit "/hobbyists/#{@hobbyist2.id}"
@@ -63,7 +63,34 @@ RSpec.describe "Hobbyists Show", type: :feature do
         click_link "Bob's Projects"
         expect(page).to have_current_path("/hobbyists/#{@hobbyist2.id}/projects")
       end
+
+      
+      # User Story 19, Parent Delete 
+      
+      # As a visitor
+      # When I visit a parent show page
+      # Then I see a link to delete the parent
+      # When I click the link "Delete Parent"
+      # Then a 'DELETE' request is sent to '/parents/:id',
+      # the parent is deleted, and all child records are deleted
+      # and I am redirected to the parent index page where I no longer see this parent
+
+      describe "Then I see a link to delete the hobbyist" do
+        describe "when I click the link a 'DELETE' request is sent to '/hobbyists/:id'" do
+          it "The hobbyist is then deleted along with all projects, and I am redirected back to the hobbyists index" do
+            visit "/hobbyists/#{@hobbyist2.id}"
+            expect(page).to have_link("Delete #{@hobbyist2.name}", href: "/hobbyists/#{@hobbyist2.id}")
+            click_link("Delete #{@hobbyist2.name}")
+            expect(current_path).to eq("/hobbyists")
+            expect(page).to_not have_content("#{@hobbyist2.name}")
+            visit "/projects"
+            expect(page).to_not have_content("#{@project2.project_name}")
+            expect(page).to_not have_content("#{@project3.project_name}")
+          end
+        end
+      end
     end
-  end
+
+
 
 end
